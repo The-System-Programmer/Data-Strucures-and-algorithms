54fbc7bf0815499d95110859029b9c510fed64c7
The-System-Programmer <the.system.programmer.2004@gmail.com>
2026-04-19 10:30:00 +0530

    Bubble sort

---
+++ Brute Force/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+void bubbleSort(vector<int>& arr,int n)
+{
+    for (int i = 0 ; i < n -1 ; i++)
+    {
+        bool swapped = false;
+        for (int j = 0 ; j < n-1 ; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+                swapped = true;
+            } 
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {64,34,25,12,22,11,90};
+    int n = arr.size();
+    bubbleSort(arr,n);
+
+    for (int x : arr)
+        cout << x << " ";
+    return 0;
+}
