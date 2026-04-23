71ee8138580dae6543cf51306b21fedbdf27e85f
The-System-Programmer <the.system.programmer.2004@gmail.com>
2026-04-23 22:38:48 +0530

    matrix multiplication

--- Brute Force/MatrixMultiplication.cpp
+++ Brute Force/MatrixMultiplication.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <cstdlib>
 
 #define n 10
@@ -14,14 +13,29 @@ int main()
         for (int j = 0 ; j < n ; j++)
         {
             a[i][j] = rand()%10;
-            b[i][j] = rand()%10; 
+            b[i][j] = rand()%10;
         }
     }
 
     for (int i = 0; i < n; i++)
+    {
         for (int j = 0; j < n; j++)
+        {
             for (int k = 0; k < n; k++)
+            {
                 c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for (int i = 0 ; i < n ; i ++)
+    {
+        for (int j = 0 ; j < n ; j++ )
+        {
+            cout << c[i][j] ;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
